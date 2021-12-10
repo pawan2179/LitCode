3a2e448b6b3891cc0d9e83118a57e1865734c8fe
pawan2179 <pawansingh4418@gmail.com>
2021-12-10 22:16:05 +0530

    Added CodeX support

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const axios = require('axios');
 
 const { generateFile } = require("./generateFile");
 const { executeCpp } = require("./executeCpp");
+const { response } = require("express");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,57 +16,53 @@ app.set("view engine", "ejs");
 
 let output="";
 let code="";
+let inputData= "";
 
 app.get('/', (req, res) => {
-    // output = output.trim();
-    // code = code.trim();
-    res.render("index", {outputText: output, codeText: code});
-    //res.sendFile(__dirname+"/index.html");
+    res.render("index", {outputText: output, codeText: code, inputText: inputData});
 });
 
 
-/*
-
-const codeArea = document.getElementById("code")
-const shareBtn = document.getElementsByClassName("share");
-//const run = runBtn[0];
-
-function getCode() {
-    
-}
-
-document.getElementById("runBtn").onclick= function() {
-    var code = codeArea.value;
-    console.log(code);
-    if(code === undefined || code.length === 0) {
-        alert("Invalid Attempt!!");
-    }
-    else {
-        
-    }
-}
-*/
-
 app.post('/', async(req, res) => {
 
-    // const { language = "cpp", code } = req.body.codeText; 
     const language = "cpp";
     code = req.body.codeText;
 
-    //console.log(code);
+    inputData = req.body.inputText
 
     if(code === undefined) {
         return res.status(400).json({ success: false, error : "Empty code body" });
     }
 
-    //Generate cpp file
-    const filePath = await generateFile(language, code);
-    // Run the file and send the response
-    //console.log(filePath);
-    output = await executeCpp(filePath);
+    var data = JSON.stringify({
+        "code": code,
+        "language":language,
+        "input": inputData
+        });
+
+    var config = {
+        method: 'post',
+        url: 'https://codexweb.netlify.app/.netlify/functions/enforceCode',
+        headers: { 
+          'Content-Type': 'application/json'
+        },
+        data : data
+      };
+
+      axios(config)
+      .then(function (response) {
+          //console.log(`${response.data.output}`)
+          output = response.data.output
+          res.redirect('/')
+      })
+      .catch(function (error) {
+          //console.log(`${error}`)
+          output = error
+          res.redirect('/')
+      });
 
     
-    res.redirect('/');
+    //res.redirect('/');
     //return res.json({filePath: filePath, output: output});
 });
 
